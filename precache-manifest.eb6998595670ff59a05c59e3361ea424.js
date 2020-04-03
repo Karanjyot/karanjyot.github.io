4d7e5695de838e04f15ea5f2a1ee790203826a8a
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-04-02 20:56:43 -0400

    Updates

---
+++ precache-manifest.eb6998595670ff59a05c59e3361ea424.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88a4f98371237a064c0b009e46642f5",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "ddaedc495ea22872f81b",
+    "url": "/reactportfolio/static/css/main.d211ace2.chunk.css"
+  },
+  {
+    "revision": "0549d04c814058a20e96",
+    "url": "/reactportfolio/static/js/2.b597d8f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactportfolio/static/js/2.b597d8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddaedc495ea22872f81b",
+    "url": "/reactportfolio/static/js/main.bc45dcdd.chunk.js"
+  },
+  {
+    "revision": "f8ca34d2a84711bf872c",
+    "url": "/reactportfolio/static/js/runtime-main.0c1598a4.js"
+  },
+  {
+    "revision": "8db136a38b00935d9969b455e307a5c1",
+    "url": "/reactportfolio/static/media/contact.8db136a3.png"
+  },
+  {
+    "revision": "0e9d51cc7645ae60468ed431e57179b3",
+    "url": "/reactportfolio/static/media/devitanswer.0e9d51cc.JPG"
+  },
+  {
+    "revision": "1dda915c0afa9e16a631bf2773a3d27c",
+    "url": "/reactportfolio/static/media/devitchat.1dda915c.JPG"
+  },
+  {
+    "revision": "c98d0384e1abe1a1270cc7fd5a302ad4",
+    "url": "/reactportfolio/static/media/devithome.c98d0384.JPG"
+  },
+  {
+    "revision": "2a4c80850a7027ca7a0be5934954ca6c",
+    "url": "/reactportfolio/static/media/devithomepage2.2a4c8085.png"
+  },
+  {
+    "revision": "29cd74748345d969da5a58cb4dfac098",
+    "url": "/reactportfolio/static/media/deviticons.29cd7474.png"
+  },
+  {
+    "revision": "dba8073fc4a40364be49098cd6429b61",
+    "url": "/reactportfolio/static/media/devitlogin.dba8073f.JPG"
+  },
+  {
+    "revision": "df2b17026d53b6c8482630bc795d698d",
+    "url": "/reactportfolio/static/media/devitquestion.df2b1702.JPG"
+  },
+  {
+    "revision": "d07b92235374ed0b24348efcdde40903",
+    "url": "/reactportfolio/static/media/devitsignup.d07b9223.JPG"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/reactportfolio/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "21cdd95727c15d4f4ef2887ede16c49b",
+    "url": "/reactportfolio/static/media/password2.21cdd957.jpg"
+  },
+  {
+    "revision": "c4b0ccc080ddb426fabaa73011da1818",
+    "url": "/reactportfolio/static/media/passwordgen.c4b0ccc0.JPG"
+  },
+  {
+    "revision": "84fc1f25b4a90e619ead117515cbee78",
+    "url": "/reactportfolio/static/media/passwordhome.84fc1f25.JPG"
+  },
+  {
+    "revision": "5ae1675d0883284b4688989cc4490409",
+    "url": "/reactportfolio/static/media/quiz2.5ae1675d.jpg"
+  },
+  {
+    "revision": "2598dd7fed3115c1989afa8c3984c504",
+    "url": "/reactportfolio/static/media/quizhs.2598dd7f.JPG"
+  },
+  {
+    "revision": "33b53065d06c472039530b0f8e4835f2",
+    "url": "/reactportfolio/static/media/quizquestion.33b53065.JPG"
+  },
+  {
+    "revision": "0026e846801cdcf5dca92b6670886e3d",
+    "url": "/reactportfolio/static/media/quizstart.0026e846.JPG"
+  },
+  {
+    "revision": "dae9c0babde2b0295667c4a9d984fa61",
+    "url": "/reactportfolio/static/media/whereto2.dae9c0ba.jpg"
+  },
+  {
+    "revision": "ae95bcc4a81ea75751caeb18bde00a90",
+    "url": "/reactportfolio/static/media/wheretohome.ae95bcc4.JPG"
+  },
+  {
+    "revision": "1f6fce00387d60381c7b7ddbeb26bbd6",
+    "url": "/reactportfolio/static/media/wheretoicons.1f6fce00.png"
+  },
+  {
+    "revision": "0e9f1fbb035c0d83a436e2f4173c359b",
+    "url": "/reactportfolio/static/media/wheretotoronto.0e9f1fbb.JPG"
+  },
+  {
+    "revision": "9cddcaf9f7d4adc25a44cc17da107754",
+    "url": "/reactportfolio/static/media/wheretotoronto2.9cddcaf9.JPG"
+  }
+]);
