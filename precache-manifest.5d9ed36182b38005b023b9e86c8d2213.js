ffd7c6c1714ecfc4ed62dcfe86a9002e349ba48b
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-05-24 14:17:48 -0400

    Updates

---
+++ precache-manifest.5d9ed36182b38005b023b9e86c8d2213.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a32fbb38d6ad3302499a2d025423083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad6613ceaa73c0bec06",
+    "url": "/static/css/main.77b3aa8c.chunk.css"
+  },
+  {
+    "revision": "35c4f1cf9c2cbf011103",
+    "url": "/static/js/2.a3a78615.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3a78615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad6613ceaa73c0bec06",
+    "url": "/static/js/main.fb948868.chunk.js"
+  },
+  {
+    "revision": "e32b3f3044b5b3e8cd07",
+    "url": "/static/js/runtime-main.f764c94e.js"
+  },
+  {
+    "revision": "8db136a38b00935d9969b455e307a5c1",
+    "url": "/static/media/contact.8db136a3.png"
+  },
+  {
+    "revision": "0e9d51cc7645ae60468ed431e57179b3",
+    "url": "/static/media/devitanswer.0e9d51cc.JPG"
+  },
+  {
+    "revision": "1dda915c0afa9e16a631bf2773a3d27c",
+    "url": "/static/media/devitchat.1dda915c.JPG"
+  },
+  {
+    "revision": "c98d0384e1abe1a1270cc7fd5a302ad4",
+    "url": "/static/media/devithome.c98d0384.JPG"
+  },
+  {
+    "revision": "2a4c80850a7027ca7a0be5934954ca6c",
+    "url": "/static/media/devithomepage2.2a4c8085.png"
+  },
+  {
+    "revision": "29cd74748345d969da5a58cb4dfac098",
+    "url": "/static/media/deviticons.29cd7474.png"
+  },
+  {
+    "revision": "dba8073fc4a40364be49098cd6429b61",
+    "url": "/static/media/devitlogin.dba8073f.JPG"
+  },
+  {
+    "revision": "df2b17026d53b6c8482630bc795d698d",
+    "url": "/static/media/devitquestion.df2b1702.JPG"
+  },
+  {
+    "revision": "d07b92235374ed0b24348efcdde40903",
+    "url": "/static/media/devitsignup.d07b9223.JPG"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "21cdd95727c15d4f4ef2887ede16c49b",
+    "url": "/static/media/password2.21cdd957.jpg"
+  },
+  {
+    "revision": "c4b0ccc080ddb426fabaa73011da1818",
+    "url": "/static/media/passwordgen.c4b0ccc0.JPG"
+  },
+  {
+    "revision": "84fc1f25b4a90e619ead117515cbee78",
+    "url": "/static/media/passwordhome.84fc1f25.JPG"
+  },
+  {
+    "revision": "5ae1675d0883284b4688989cc4490409",
+    "url": "/static/media/quiz2.5ae1675d.jpg"
+  },
+  {
+    "revision": "2598dd7fed3115c1989afa8c3984c504",
+    "url": "/static/media/quizhs.2598dd7f.JPG"
+  },
+  {
+    "revision": "33b53065d06c472039530b0f8e4835f2",
+    "url": "/static/media/quizquestion.33b53065.JPG"
+  },
+  {
+    "revision": "0026e846801cdcf5dca92b6670886e3d",
+    "url": "/static/media/quizstart.0026e846.JPG"
+  },
+  {
+    "revision": "dae9c0babde2b0295667c4a9d984fa61",
+    "url": "/static/media/whereto2.dae9c0ba.jpg"
+  },
+  {
+    "revision": "ae95bcc4a81ea75751caeb18bde00a90",
+    "url": "/static/media/wheretohome.ae95bcc4.JPG"
+  },
+  {
+    "revision": "1f6fce00387d60381c7b7ddbeb26bbd6",
+    "url": "/static/media/wheretoicons.1f6fce00.png"
+  },
+  {
+    "revision": "0e9f1fbb035c0d83a436e2f4173c359b",
+    "url": "/static/media/wheretotoronto.0e9f1fbb.JPG"
+  },
+  {
+    "revision": "9cddcaf9f7d4adc25a44cc17da107754",
+    "url": "/static/media/wheretotoronto2.9cddcaf9.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactportfolio/precache-manifest.eb6998595670ff59a05c59e3361ea424.js"
+  "/precache-manifest.5d9ed36182b38005b023b9e86c8d2213.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
